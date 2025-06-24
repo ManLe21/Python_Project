cafb2c0e7d7f00ac2020eb8e480ccc5c34c78a0d
ManLe21 <manik_minasyan15@alumni.aua.am>
2025-06-24 21:34:55 +0400

    Message

---
+++ Week1/Pyth.py
@@ -0,0 +1,10 @@
+import sys
+
+number1 = int(sys.argv[1])
+number2 = int(sys.argv[2])
+
+if number1 > number2:
+    print("The bigger number is:", number1)
+else:
+    print("The bigger number is:", number2)
+
